e994a4bbbe961749fd7af6567cfd30392d05c7c7
mark jayson esplana <mark.esplana514@gmail.com>
2026-08-20 18:17:12 +0800

    chore: bump MySQL engine version to 8.4
    Avoids automatic upgrade after MySQL 8.0 end of standard support.

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -61,7 +61,7 @@ resource "aws_db_instance" "rds" {
   identifier = "catalog"
 
   engine         = "mysql"
-  engine_version = "8.0"
+  engine_version = "8.4"
   instance_class = var.instance_class
 
   allocated_storage = var.allocated_storage
